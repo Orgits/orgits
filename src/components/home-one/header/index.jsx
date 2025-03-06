72805c165510078ed07f17fc4ecf925830510823
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2025-03-06 20:33:18 +0530

    ok

--- src/components/home-one/header/index.jsx
+++ src/components/home-one/header/index.jsx
@@ -16,8 +16,10 @@ function Header() {
 					<HeaderLogo />
 					<div className="menu-block-wrapper">
 						<DesktopNav>
-							<NavItem dropdown title="For Businesses">
-								<Dropdown>
+						<NavItem url="/">Home</NavItem>
+							<NavItem url="about-us">About Us</NavItem>
+							<NavItem dropdown title="Services">
+							<Dropdown>
 									<DropdownItem url="home-two">Zoho | Hubspot | Salesforce</DropdownItem>
 									<DropdownItem url="home-three">SEO Optimization</DropdownItem>
 									<DropdownItem url="home-four">Business Consultation</DropdownItem>
@@ -26,61 +28,9 @@ function Header() {
 									<DropdownItem url="home-seven">Advertising agency</DropdownItem>
 								</Dropdown>
 							</NavItem>
-							<NavItem url="about-us">About Us</NavItem>
-							<NavItem dropdown title="Pages">
-								<Dropdown>
-									<DropdownItem url="about-us">About Us</DropdownItem>
-									<DropdownItem url="pricing">Pricing</DropdownItem>
-									<DropdownItem dropdown title="Blog">
-										<Dropdown noShape>
-											<DropdownItem url="blog">Our Blog</DropdownItem>
-											<DropdownItem url="blog-grid">Blog Grid</DropdownItem>
-											<DropdownItem url="single-blog">Blog Details</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-									<DropdownItem dropdown title="Service">
-										<Dropdown noShape>
-											<DropdownItem url="service">Service</DropdownItem>
-											<DropdownItem url="single-service">Service Details</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-									<DropdownItem dropdown title={"Team"}>
-										<Dropdown noShape>
-											<DropdownItem url="team">Team</DropdownItem>
-											<DropdownItem url="single-team">Team Details</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-									<DropdownItem dropdown title={"Portfolio"}>
-										<Dropdown noShape>
-											<DropdownItem url="portfolio-one">Portfolio One Column</DropdownItem>
-											<DropdownItem url="portfolio-two">Portfolio Two Column</DropdownItem>
-											<DropdownItem url="single-portfolio">Portfolio Details</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-									<DropdownItem dropdown title="Utility">
-										<Dropdown noShape>
-											<DropdownItem url="faq">FAQ</DropdownItem>
-											<DropdownItem url="error-page">Error Page</DropdownItem>
-											<DropdownItem url="testimonial">Testimonial</DropdownItem>
-											<DropdownItem url="coming-soon">Coming Soon</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-									<DropdownItem dropdown title="Account">
-										<Dropdown noShape>
-											<DropdownItem url="sign-up">Sign Up</DropdownItem>
-											<DropdownItem url="sign-in">Sign In</DropdownItem>
-											<DropdownItem url="reset-password">Reset Password</DropdownItem>
-										</Dropdown>
-									</DropdownItem>
-								</Dropdown>
-							</NavItem>
-							<NavItem dropdown title="Blog">
-								<Dropdown>
-									<DropdownItem url="blog">Our Blog</DropdownItem>
-									<DropdownItem url="blog-grid">Blog Grid</DropdownItem>
-									<DropdownItem url="single-blog">Blog Details</DropdownItem>
-								</Dropdown>
-							</NavItem>
+						
+							<NavItem url="blog-grid">Blogs</NavItem>
+							
 							<NavItem url="contact-us">Contact Us</NavItem>
 						</DesktopNav>
 					</div>
